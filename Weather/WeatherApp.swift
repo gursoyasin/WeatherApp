73976a0ea7e2c5ea7125097ccc953491bffded91
gursoyasin <gursoyreal@gmail.com>
2023-08-08 11:00:03 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Yasin Gürsoy on 8.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
