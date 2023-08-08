ffdcd765dc979da0253e33b0b32449ac68522a99
gursoyasin <gursoyreal@gmail.com>
2023-08-08 14:37:50 +0300

    Finish

---
+++ Weather/Views/WelcomeView.swift
@@ -0,0 +1,51 @@
+//
+//  WelcomeView.swift
+//  Weather
+//
+//  Created by Yasin Gürsoy on 8.08.2023.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
+    
+    
+    var body: some View {
+        VStack{
+            VStack(spacing: 20) {
+                
+                Text("Hava Durumu")
+                    .bold().font(.title)
+                
+                
+                Text("Bölgenizdeki hava durumunu öğrenmek için lütfen mevcut konumunuzu paylaşın")
+                    .padding()
+                
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+            
+            
+            
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
